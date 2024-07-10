18c019b3011774bb5491ce1968334b3a8f31df37
OfflineBrain <leonidivakhnenko@gmail.com>
2024-07-10 18:26:26 +0900

    shadow jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
 plugins {
     kotlin("jvm") version "2.0.0"
+    application
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "io.github.offlinebrain"
 version = "1.0-SNAPSHOT"
+
+val mainClassName = "io.github.offlinebrain.MainKt"
 
 repositories {
     mavenCentral()
@@ -18,4 +22,8 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+
+application {
+    mainClass = mainClassName
 }
